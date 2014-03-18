67dc974460044eac05a135b51831e1674a8ebd24
Caleb Saunders <csaunders1@toromail.csudh.edu>
2014-03-18 14:18:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Ruby Tunes
+
+
+License: MIT License
+
+
+###To-do
+
+    Add a label which will display the current playing track.
+    Add oAuth login + Twitter API to tweet current track
+    Add a shuffle feature to play an extremely random track
+   
---
+++ ruby_tunes.rb
@@ -0,0 +1,52 @@
+require 'win32ole'
+require 'tk'
+
+@app = WIN32OLE.new('iTunes.Application')
+
+ $currentTrackvar = TkVariable.new
+ def pause
+    
+ @app = WIN32OLE.new('iTunes.Application')
+    @app.Pause
+    @app.BrowserWindow.MiniPlayer = true
+  end
+  
+ def play
+   @app = WIN32OLE.new('iTunes.Application')
+  @app.Play
+  @app.BrowserWindow.MiniPlayer = false
+ end
+
+
+root = TkRoot.new { title "Ruby Tunes" }
+play_button = TkButton.new(root) do
+    text "Play"
+    borderwidth 5
+    underline 0
+    state "normal"
+    cursor "sailboat"
+    font TkFont.new( 'times 20 bold')
+    foreground "red"
+    activebackground "blue"
+    relief "groove"
+    command (proc  {play})
+    pack("side"=> "right", "padx"=> "5", "pady"=>"5")
+   end
+   
+   
+//Pause the current playing track
+ pause_button = TkButton.new(root) do
+    text "Pause"
+    borderwidth 5
+    underline 0
+    state "normal"
+    cursor "umbrella"
+    font TkFont.new( 'times 20 bold')
+    foreground "red"
+    activebackground "blue"
+    relief "groove"
+    command (proc  {pause})
+    pack("side"=> "left", "padx"=> "5", "pady"=>"5")
+   end
+ Tk.mainloop
+
